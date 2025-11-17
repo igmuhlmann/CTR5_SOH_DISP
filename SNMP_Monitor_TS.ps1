af91c38bf7bf4a69eb1e6d50bf0f0f624df90c96
igmuhlmann <75082429+igmuhlmann@users.noreply.github.com>
2025-11-17 15:00:41 +0100

    Change update interval to 60 seconds and add state checks
    Updated the SNMP monitor script to change the update interval from 5 seconds to 60 seconds and added case statements for additional state checks.

--- SNMP_Monitor_TS.ps1
+++ SNMP_Monitor_TS.ps1
@@ -7,7 +7,7 @@ $ip = "192.168.178.88"                        # SNMP device IP
 $community = "public"                         # Change if necessary
 $csvFile = "C:\Users\s.uhlmann\CTR_SNMP_OID.csv"  # Input file: Variable,OID
 $outFile = "C:\Users\s.uhlmann\CTR_SNMP_Output.txt" # Output file
-$intervalSeconds = 5                          # Update interval in seconds
+$intervalSeconds = 60                          # Update interval in seconds
 
 function Get-FirmwareVersion {
     param(
@@ -70,11 +70,80 @@ while ($true) {
             if ($result) {
                 $value = $result
 
+
                 # Optional decoding of firmware version
                 if ($oid -eq '.1.3.6.1.4.1.58765.1.2.10.0') {
                     $value = Get-FirmwareVersion $value
-					Write-Host "does work"
                 }
+				
+				# Case Statements to add the state
+				
+				# nmxCentaurInstrumentState
+				if ($oid -eq '.1.3.6.1.4.1.58765.1.2.8.0') {
+					switch ($value) {
+					0 { $value = "0: ok" }
+					1 { $value = "1: warning" }
+					2 { $value = "2: error" }
+					}
+				}
+				
+				# nmxCentaurCommitState
+				if ($oid -eq '.1.3.6.1.4.1.58765.1.2.4.0') {
+					switch ($value) {
+					0 { $value = "0: comitted" }
+					1 { $value = "1: not committed" }
+					}
+				}
+
+				# nmxCentaurGnssState
+				if ($oid -eq '.1.3.6.1.4.1.58765.1.1.3.0') {
+					switch ($value) {
+					0 { $value = "0: off" }
+					1 { $value = "1: unlocked" }
+					2 { $value = "2: locked" }
+					}
+				}				
+				
+				# nmxCentaurTimingPLLState
+				if ($oid -eq '.1.3.6.1.4.1.58765.1.1.1.0') {
+					switch ($value) {
+					0 { $value = "0: noLock" }
+					1 { $value = "1: coarseLock" }
+					2 { $value = "2: fineLock" }
+					3 { $value = "2: freeRunning" }
+					}
+				}			
+
+				# nmxCentaurTimeState
+				if ($oid -eq '.1.3.6.1.4.1.58765.1.1.4.0') {
+					switch ($value) {
+					0 { $value = "0: timeOK" }
+					1 { $value = "1: freeRunning" }
+					2 { $value = "2: init" }
+					3 { $value = "3: timeError" }
+					4 { $value = "4: timeServerUnreachable" }
+					5 { $value = "4: noAntenna" }
+					}
+				}								
+				
+				if ($oid -eq '.1.3.6.1.4.1.58765.1.3.1.0') {
+					switch ($value) {
+					0 { $value = "0: ok" }
+					1 { $value = "1: warning" }
+					2 { $value = "2: error" }
+					}
+				}
+				
+				if ($oid -eq '.1.3.6.1.4.1.58765.1.3.2.0') {
+					switch ($value) {
+					0 { $value = "0: ok" }
+					1 { $value = "1: warning" }
+					2 { $value = "2: error" }
+					}
+				}
+
+				#Write-Host $oid "#" $value
+
 
                 # -------------------------------
                 # VALUE CHANGE TRACKING LOGIC
